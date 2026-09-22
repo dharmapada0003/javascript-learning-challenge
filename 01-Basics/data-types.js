f79944f25e03468786a7cddbd2e3fb53adf95422
dharmapada0003 <dharmapada789@gmail.com>
2026-09-22 22:50:20 +0530

    learned variables & data types

---
+++ 01-Basics/data-types.js
@@ -0,0 +1,45 @@
+//Data Types :- Primitive & Non-primitive data types
+
+//Primitive Data Types
+
+//Number: (Might be +ve or -ve)
+let num = 30;
+
+const price = 100;
+
+//String
+const name = "Dharmapada";
+
+//Boolean
+isFollow = true;
+
+//Null
+let y = null;
+
+//Bigint
+let x = BigInt("1234");
+console.log(x);
+
+//Symbol
+let z = Symbol("Hii");
+console.log(z);
+
+//Non-rimitive Data Types
+
+//Objects:- Arrays & Functions
+
+//Objects
+const student = {
+  name: "Dharmapada",
+  age: 22,
+  cgpa: 9,
+  isPass: true,
+};
+
+console.log(student);
+
+//Arrays
+//Will learn next
+
+//Functions
+//Will learn next
---
+++ 01-Basics/variables.js
@@ -0,0 +1,11 @@
+//Variables
+
+//let
+let myAge = 25;
+myAge = 26; //Value can be updated & can be re-declared
+
+//const
+const pi = 3.4; //This value can not be updated but can not be re-declared
+
+//var (Old variable used before 2015)
+var name = "Dharmapada"; //Value can be updated & can be re-declared
